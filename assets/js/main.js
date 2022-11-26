a9b092347f050531aba77b92684d38208615fae8
Thalyson Barros <thalysonbarrospereira2019@gmail.com>
2022-11-26 18:04:58 -0300

    CHORE: Melhorando versão mobile do site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,19 @@
 var menuIcon = document.querySelector('.nav-menuicon');
 var navUl = document.querySelector('.nav-ul');
+var navText = document.querySelector('.navmenu-text');
 
 menuIcon.addEventListener('click', () => {
 
     if (navUl.classList.contains('active')) {
 
         navUl.classList.remove('active');
+        navText.classList.remove('active');
         document.querySelector('.nav-menuicon img').src = 'assets/img/menu.png';
 
     } else {
 
         navUl.classList.add('active');
+        navText.classList.add('active');
         document.querySelector('.nav-menuicon img').src = 'assets/img/close.png';
 
     }
